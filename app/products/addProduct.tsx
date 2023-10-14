9ffe0798b984fe099946ed04baba23fd41b84a90
kemaltf <kemaltaufikf@gmail.com>
2023-10-14 16:26:23 +0700

    disable button while updating the data

--- app/products/addProduct.tsx
+++ app/products/addProduct.tsx
@@ -5,9 +5,11 @@ export default function AddProduct() {
   const [title, setTitle] = useState('');
   const [price, setPrice] = useState('');
   const [modal, setModal] = useState(false);
+  const [isMutating, setIsMutating] = useState(false);
   const router = useRouter();
   async function handleSubmit(e: SyntheticEvent) {
     e.preventDefault();
+    setIsMutating(true);
     await fetch('http://localhost:5000/products', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -16,6 +18,7 @@ export default function AddProduct() {
         price: price,
       }),
     });
+    setIsMutating(false);
     setTitle('');
     setPrice('');
     router.refresh();
@@ -47,9 +50,15 @@ export default function AddProduct() {
               <button type="button" className="btn" onClick={handleChange}>
                 Close
               </button>
-              <button type="submit" className="btn btn-primary">
-                Save
-              </button>
+              {!isMutating ? (
+                <button type="submit" className="btn btn-primary">
+                  Save
+                </button>
+              ) : (
+                <button type="button" className="btn loading">
+                  Saving...
+                </button>
+              )}
             </div>
           </form>
         </div>
