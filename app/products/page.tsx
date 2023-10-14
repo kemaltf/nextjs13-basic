4f4ed80f8374711f6f07c9ddedc23e459c288cf8
kemaltf <kemaltaufikf@gmail.com>
2023-10-14 15:11:17 +0700

    creating products page with table (daisyui and tailwinds) and introducing fetch data in next js 13.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,37 @@
-export default function ProductsList() {
-  return <div>Products List</div>;
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+};
+async function getProducts() {
+  const res = await fetch('http://localhost:5000/products');
+  return res.json();
+}
+
+export default async function ProductsList() {
+  const product: Product[] = await getProducts();
+  return (
+    <div className="py-10 px-10">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {product.map((product, index) => (
+            <tr key={product.id}>
+              <td>{index + 1}</td>
+              <td>{product.title}</td>
+              <td>{product.price}</td>
+              <td>Actions</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
